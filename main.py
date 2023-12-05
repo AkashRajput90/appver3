cc3f8d73b2fc8a4ee8fbf09d960f23e9f9271454
Akash Manhas <122945144+AkashRajput90@users.noreply.github.com>
2023-12-05 15:56:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = [
+    {'id': 1, 'title': 'Task 1', 'description': 'Description for Task 1'},
+    {'id': 2, 'title': 'Task 2', 'description': 'Description for Task 2'},
+    # Add more tasks here
+]
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', tasks=tasks)
 
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        # Add your logic to add a new task to the tasks list
+        new_task = {'id': len(tasks) + 1, 'title': title, 'description': description}
+        tasks.append(new_task)
+        return redirect(url_for('index'))
+    return render_template('add_task.html')
+
+@app.route('/delete_task/<int:task_id>')
+def delete_task(task_id):
+    global tasks
+    tasks = [task for task in tasks if task['id'] != task_id]
+    return redirect(url_for('index'))
+
+@app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
+def edit_task(task_id):
+    task_to_edit = [task for task in tasks if task['id'] == task_id][0]
+    if request.method == 'POST':
+        task_to_edit['title'] = request.form['title']
+        task_to_edit['description'] = request.form['description']
+        # Add your logic to update the task in the tasks list
+        return redirect(url_for('index'))
+    return render_template('edit_task.html', task=task_to_edit)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=5000)
